b9f73e600a288802363d244d32015c636e90f297
earthshine0 <earthshinelynx@gmail.com>
2016-07-18 02:21:01 -0700

    Add buttons to main menu

--- app/src/main/java/com/arenjae/tictactoe/MainMenu.java
+++ app/src/main/java/com/arenjae/tictactoe/MainMenu.java
@@ -2,13 +2,20 @@ package com.arenjae.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+
+    Button btnNewGame;
+    Button btnNewGameCPU;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        btnNewGame = (Button)findViewById(R.id.btnNewGame);
+        btnNewGameCPU = (Button)findViewById(R.id.btnNewGameCPU);
     }
 
 }
