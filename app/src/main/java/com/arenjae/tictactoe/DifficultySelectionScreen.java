9a08c41f1c7ec01132789a86439d9908556f756f
earthshine0 <earthshinelynx@gmail.com>
2016-07-17 21:35:45 -0700

    Added Difficulty Selection screen

---
+++ app/src/main/java/com/arenjae/tictactoe/DifficultySelectionScreen.java
@@ -0,0 +1,13 @@
+package com.arenjae.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DifficultySelectionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difficulty_selection_screen);
+    }
+}
--- app/src/main/java/com/arenjae/tictactoe/MainMenu.java
+++ app/src/main/java/com/arenjae/tictactoe/MainMenu.java
@@ -1,7 +1,9 @@
 package com.arenjae.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
@@ -16,6 +18,22 @@ public class MainMenu extends AppCompatActivity {
 
         btnNewGame = (Button)findViewById(R.id.btnNewGame);
         btnNewGameCPU = (Button)findViewById(R.id.btnNewGameCPU);
+
+        btnNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),GameScreen.class);
+                startActivity(i);
+            }
+        });
+
+        btnNewGameCPU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),DifficultySelectionScreen.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
