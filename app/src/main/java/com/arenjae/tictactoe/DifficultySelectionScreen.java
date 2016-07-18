1ba8dbb808609dc02a7c8233d755c1326d377c82
earthshine0 <earthshinelynx@gmail.com>
2016-07-18 15:04:17 -0700

    change theme to material dark

--- app/src/main/java/com/arenjae/tictactoe/DifficultySelectionScreen.java
+++ app/src/main/java/com/arenjae/tictactoe/DifficultySelectionScreen.java
@@ -1,13 +1,36 @@
 package com.arenjae.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DifficultySelectionScreen extends AppCompatActivity {
+
+    Button btnEasy;
+    Button btnNormal;
+    Button btnHard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_difficulty_selection_screen);
+
+        btnEasy = (Button)findViewById(R.id.btnEasy);
+        btnNormal = (Button)findViewById(R.id.btnNormal);
+        btnHard = (Button)findViewById(R.id.btnHard);
+
+        btnEasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),GameScreen.class);
+                startActivity(i);
+            }
+        });
+
     }
+
+
+
 }
